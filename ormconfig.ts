7224a9d6a6c9343ebb150c442cc4b325fbae645b
Mohannad Ebrahim Alhatame <mr.m0ohannad@gmail.com>
2024-11-11 19:14:55 +0300

    setup projrct with typeORM & pgDB

---
+++ ormconfig.ts
@@ -0,0 +1,24 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: '',
+  database: 'masar-blog-db',
+  // entities: [Episode],
+  //   autoLoadEntities: true,
+  synchronize: true, // todo: not safe for production and we should use migrations instead
+  // subscribers: [__dirname + '/domain/subscribers/*.subscriber{.ts,.js}'],
+  migrations: ['src/migration/*{.ts,.js}'],
+  entities: ['src/**/*.entity{.ts,.js}'],
+};
+
+const dataSource = new DataSource(dataSourceOptions);
+dataSource.initialize();
+
+export default dataSource;
+
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+const entitiesPath = __dirname + '/**/*.entity{.ts,.js}';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '',
+      database: 'masar-blog-db',
+      autoLoadEntities: true,
+      entities: [entitiesPath],
+      synchronize: false,
+      logging: false,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
